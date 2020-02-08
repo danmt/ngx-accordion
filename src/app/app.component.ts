cf99e6f822fe487fb6e72bea5b22877c94216665
Daniel Marin <danielarturomt@gmail.com>
2020-02-07 22:58:38 -0300

    a few improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,46 @@
-import { Component, ViewChildren, QueryList, ElementRef } from '@angular/core';
+import {
+  Component,
+  ViewChildren,
+  QueryList,
+  ElementRef,
+  AfterViewInit
+} from '@angular/core';
 import { AccordionHeaderComponent } from './shared/components/accordion-header/accordion-header.component';
+
+const ARROW_DOWN = 'ArrowDown';
+const ARROW_UP = 'ArrowUp';
+const HOME = 'Home';
+const END = 'End';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   @ViewChildren(AccordionHeaderComponent) accordionHeaders: QueryList<
     ElementRef
   >;
   focus = false;
   currentlyFocused = -1;
   currentlyOpen = -1;
+  private panelQuantity: number;
+
+  ngAfterViewInit() {
+    this.panelQuantity = this.accordionHeaders.length;
+  }
+
+  private goToNext() {
+    this.currentlyFocused = (this.currentlyFocused + 1) % this.panelQuantity;
+  }
+
+  private goToPrevious() {
+    if (this.currentlyFocused === 0) {
+      this.currentlyFocused = this.panelQuantity - 1;
+    } else {
+      this.currentlyFocused = this.currentlyFocused - 1;
+    }
+  }
 
   setFocus(pos: number) {
     this.focus = true;
@@ -30,5 +58,24 @@ export class AppComponent {
 
   closePanel() {
     this.currentlyOpen = -1;
+  }
+
+  handleKeyUp(event: KeyboardEvent) {
+    switch (event.key) {
+      case ARROW_DOWN:
+        this.goToNext();
+        break;
+      case ARROW_UP:
+        this.goToPrevious();
+        break;
+      case HOME:
+        this.currentlyFocused = 0;
+        return;
+      case END:
+        this.currentlyFocused = this.panelQuantity - 1;
+        return;
+      default:
+        break;
+    }
   }
 }
